6581373d5319dba5a0096439e00bb4b07bcc6cf1
anna sent <anna.sent@gmail.com>
2015-03-24 23:27:35 +0400

    display text in text view

--- src/com/anna/sent/soft/devicescreeninfotest/MainActivity.java
+++ src/com/anna/sent/soft/devicescreeninfotest/MainActivity.java
@@ -1,15 +1,30 @@
 package com.anna.sent.soft.devicescreeninfotest;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.anna.sent.soft.utils.DisplayUtils;
 
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		StringBuilder sb = new StringBuilder();
+		Point sizeInDp = DisplayUtils.getScreenSizeInDpWrapped(this);
+		Point sizeInPx = DisplayUtils.getScreenSizeInPx(this);
+		sb.append("Width in dp = ").append(sizeInDp.x).append("\n");
+		sb.append("Height in dp = ").append(sizeInDp.y).append("\n");
+		sb.append("Width in px = ").append(sizeInPx.x).append("\n");
+		sb.append("Height in px = ").append(sizeInPx.y).append("\n");
+
+		TextView tv = (TextView) findViewById(R.id.textView);
+		tv.setText(sb.toString());
 	}
 
 	@Override
